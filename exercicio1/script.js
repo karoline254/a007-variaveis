34caa6b284ca99e2d61f2597c9ccdf7b2e5dd9d9
karoline254 <karolineexecut@gmail.com>
2022-08-29 14:59:35 -0300

    Exercicio01

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+
+//const nome = "Karoline"
+//let idade = 27
+//console.log(nome, typeof nome, idade,  typeof idade)
+
+let nome = prompt("Qual o seu nome?")
+let idade = Number(prompt("Qual a sua idade?"))
+console.log(nome, typeof nome)
+console.log(idade, typeof idade)
+console.log("Olá, " + nome + ", você tem " + idade + " anos.")
